22cf7ccf3595bbd7dc5a5cb03fdd91639107bb97
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2020-07-30 18:26:21 +1200

    Added Default Project

---
+++ src/main/java/com/homelikebrick42/realisticmining/RealisticMining.java
@@ -0,0 +1,12 @@
+package com.homelikebrick42.realisticmining;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RealisticMining implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        
+    }
+
+}
